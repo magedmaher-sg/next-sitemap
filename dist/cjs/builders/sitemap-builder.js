8cbc94276b2bad60437a4efca0e65ac3466bfae7
Maged Maher <maged@stllr.network>
2023-02-23 16:03:18 +0200

    remove unneeded attributes in xml headers

--- dist/cjs/builders/sitemap-builder.js
+++ dist/cjs/builders/sitemap-builder.js
@@ -72,7 +72,7 @@ var SitemapBuilder = /** @class */ (function () {
      * @returns
      */
     SitemapBuilder.prototype.withXMLTemplate = function (content) {
-        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:news=\"http://www.google.com/schemas/sitemap-news/0.9\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\" xmlns:mobile=\"http://www.google.com/schemas/sitemap-mobile/1.0\" xmlns:image=\"http://www.google.com/schemas/sitemap-image/1.1\" xmlns:video=\"http://www.google.com/schemas/sitemap-video/1.1\">\n".concat(content, "</urlset>");
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\">\n".concat(content, "</urlset>");
     };
     /**
      * Generates sitemap-index.xml
